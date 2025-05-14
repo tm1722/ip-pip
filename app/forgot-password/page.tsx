adb520304fea216a06a93958dffdf18c9d994430
ChangingNickname <57766444+ChangingNickname@users.noreply.github.com>
2025-05-14 22:54:53 +0100

    Made blank pages for forgot-password and reset-password pages

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPasswordPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Forgot Password</h1>
+        </div>
+    );
+};
+
+export default ForgotPasswordPage;
---
+++ app/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetPasswordPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Reset Password</h1>
+        </div>
+    );
+};
+
+export default ResetPasswordPage;
